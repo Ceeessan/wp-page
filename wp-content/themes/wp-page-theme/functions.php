d64193d44ead75267e1ec31b472e54dbfbdf7d77
Ceeessan <Cessan__94@hotmail.com>
2025-04-05 22:47:53 +0200

    touch-ups

--- wp-content/themes/wp-page-theme/functions.php
+++ wp-content/themes/wp-page-theme/functions.php
@@ -4,11 +4,11 @@ exit;
 }
 //Initialize theme
 require_once(get_template_directory() . "/init.php");
-require_once("vite.php");
-require_once("init.php");
-function petPalace_add_woocommerce_support() {
+// require_once("vite.php");
+// require_once("init.php");
+function wp_page_add_woocommerce_support() {
 add_theme_support( 'woocommerce' );
 }
-add_action( 'after_setup_theme', 'petPalace_add_woocommerce_support' );
+add_action( 'after_setup_theme', 'wp_page_add_woocommerce_support' );
 
 ?>
--- wp-content/themes/wp-page-theme/init.php
+++ wp-content/themes/wp-page-theme/init.php
@@ -2,7 +2,7 @@
 function wp_page(){
 // Här länkar vi till CSS och JS.
 $theme_directory = get_template_directory_uri();
-wp_enqueue_style("wp-pahe", $theme_directory . "/app.scss");
+wp_enqueue_style("wp-page", $theme_directory . "/app.css");
 wp_enqueue_script("app", $theme_directory . "/app.js");
 }
 add_action('wp_enqueue_scripts', 'wp_page');
